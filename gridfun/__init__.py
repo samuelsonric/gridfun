476850ca2065411cd5814b63565ac79b380a58bd
Richard Samuelson <samuelson.rick@gmail.com>
2021-02-20 15:32:49 -0800

    added module __init__.py

---
+++ gridfun/__init__.py
@@ -0,0 +1,14 @@
+from gridfun.grid import Grid, Axis, Endpoint
+from gridfun.gridfun import (
+    GridFun,
+    add,
+    subtract,
+    maximum,
+    minimum,
+    multiply,
+    floor_divide,
+    preimg,
+    negative,
+    absolute,
+)
+from gridfun.prob import Dirac, Rows, Cols, Kernel, tensor_prod
